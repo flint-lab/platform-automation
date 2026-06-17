649b1a40b7f3d3298f79eb29cfb54418d86da39d
Anjani Kumari <anjani@flintlab.io>
2026-06-17 07:40:43 +0000

    Implement Automated login flow for flintlab.io using Playwright

---
+++ FlintUITest/UI Workflow/framework/locators/login_locators.py
@@ -0,0 +1,13 @@
+class LoginLocators:
+  
+    LANDING_LOGIN_BUTTON = "#login-button"
+
+  
+    EMAIL_PLACEHOLDER = "yours@example.com"
+    EMAIL_INPUT = 'input[type="email"]'
+    
+    PASSWORD_PLACEHOLDER = "your password"
+    PASSWORD_INPUT = 'input[type="password"]'
+
+
+    SUBMIT_BUTTON_TEXTS = ["LOG IN", "Log In", "Continue"]
